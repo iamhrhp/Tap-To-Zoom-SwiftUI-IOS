807cca3a68f7c4d79e9bfd8b0502011886f2b3da
hrhp <hrhp007@gmail.com>
2020-11-27 04:27:50 -0800

    Initial Commit

---
+++ Animatiom/AnimatiomApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatiomApp.swift
+//  Animatiom
+//
+//  Created by HRHP Parvej on 27/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AnimatiomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
